505f051a3905eb73d9d2bdd559630ab1d97d1e7a
Adrien BELFER <adrien.belfer@eleves.enpc.fr>
2021-03-27 16:12:48 +0100

    1ère allocation du travail + une fonction pour ajouter une réf au file

--- references.jl
+++ references.jl
@@ -35,10 +35,41 @@ function read_references_from_file(path::String)::Vector{Reference}
     references = [read_reference_from_string(data[i]) for i in 2:(1+R)]
     return references
 end
-<<<<<<< HEAD
+
+function add_reference_to_file(ref::Reference, path::String)
+    references = read_references_from_file(path)
+    append!(references, [ref])
+    write_references_to_file(references, path)
+end
 
 function work_needed(ref::Reference)
     return sum(ref.tasks)
 end
-=======
->>>>>>> 0997bc421250f81a0052433ab641008505306884
+
+function work_allocation(ref::Reference, nb_workers::Int)
+    # This allocation tries to minimize the maximal time spent by a worker
+    # Complexity in 2^nb_workers, to upgrade!
+    work_time_per_worker = work_needed(ref)/nb_workers
+    if nb_workers == 1
+        return ([length(ref.tasks)], [sum(ref.tasks)])
+    else
+        first_separator = 0
+        correspondant_work = 0
+        while correspondant_work < work_time_per_worker
+            first_separator += 1
+            correspondant_work += ref.tasks[first_separator]
+        end
+        allocation1 = work_allocation(Reference(ref.number, ref.tasks[first_separator:end]), nb_workers-1)
+        separators1 = append!([first_separator-1], allocation1[1])
+
+        times_1 = append!([sum(ref.tasks[1:first_separator-1])], allocation1[2])
+        allocation2 = work_allocation(Reference(ref.number, ref.tasks[first_separator+1:end]), nb_workers-1)
+        separators2 = append!([first_separator], allocation2[1])
+        times_2 = append!([sum(ref.tasks[1:first_separator])], allocation2[2])
+        if maximum(times_1) < maximum(times_2)
+            return separators1, times_1
+        else
+            return separators2, times_2
+        end
+    end
+end
