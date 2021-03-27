26b1c7c2fccf1bbee93152c8c01335ffa430748f
Adrien BELFER <adrien.belfer@eleves.enpc.fr>
2021-03-27 19:39:35 +0100

    rajout d'un main pour résumer ce qui a été fait

--- main.jl
+++ main.jl
@@ -3,4 +3,10 @@ include("production.jl")
 
 function main()
     references = read_references_from_file("./references.txt")
+    R = length(references)
+    alloc = prod_allocations_by_ref_and_nb_workers(references)
+    prods = Production(rand(1:R, 150))
+    nb_workers = 10
+    make_times_calculation(prods, alloc[:, nb_workers], nb_workers)
+    check_time(prods, alloc[:, nb_workers], nb_workers)
 end
