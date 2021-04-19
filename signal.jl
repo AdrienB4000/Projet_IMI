0d96fa7b293d8473a975c3f62deff39c0343a9f5
clemgris <clemence.grislain@eleves.enpc.fr>
2021-04-19 14:57:43 +0200

    cc

--- signal.jl
+++ signal.jl
@@ -5,13 +5,15 @@ signal:
 - Date: 2021-04-12
 =#
 
+#unité d etemps = 1 heure
+
 const nb_ref_unite_tps
 
 struct Signal
     planning::Vector{Int}
-    size::Int #en unite de temps
+    T::Int #en unite de temps
 
-    Signal(planning, size)=new(planning, size)
-    Signal(size)=new(rand(1:R, size*nb_ref_unite_tps), size)
+    Signal(planning, 1)=new(planning, 1)
+    Signal(T)=new(rand(1:R, nb_ref_unite_tps), size) #references a produire en une unité de temps dans T unité de temps
 end
 
