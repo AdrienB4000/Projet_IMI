7b502465cffe9b9dd7275918ad3164beebfac980
Hamza Zerhouni <hamza.zerhouni@eleves.enpc.fr>
2021-04-19 17:08:48 +0200

    test stocks.jl

--- signal.jl
+++ signal.jl
@@ -7,13 +7,12 @@ signal:
 
 #unité d etemps = 1 heure
 
-const nb_ref_unite_tps
+#const nb_ref_unite_tps =
 
 struct Signal
-    planning::Vector{Int}
+    planning::Array{Int64,1}
     T::Int #en unite de temps
 
-    Signal(planning, 1)=new(planning, 1)
-    Signal(T)=new(rand(1:R, nb_ref_unite_tps), size) #references a produire en une unité de temps dans T unité de temps
+    Signal(planning, T)=new(planning, T)
+    #Signal(T)=new(rand(1:R, nb_ref_unite_tps), size) #references a produire en une unité de temps dans T unité de temps
 end
-
--- stocks.jl
+++ stocks.jl
@@ -11,11 +11,17 @@ end
 
 
 function Compute_Stocks_at_T_plus_1(signal::Signal, production::Production, stocks_T::Stocks, nbr_references::Int)
-    new_stocks::Array{Int64,1}(undef,nbr_references)
+    new_stocks = Array{Int64,1}(undef,nbr_references)
     cum_prods = countmap(production.planning)
     cum_demand = countmap(signal.planning)
+
     for i in 1:nbr_references
-        new_stocks[i] = stocks_T.planning[i] + cum_prods[i] - cum_demand[i]
+        if i in keys(cum_prods)
+            new_stocks[i] = stocks_T.stocks[i] + cum_prods[i]
+        end
+        if i in keys(cum_demand)
+            new_stocks[i] = stocks_T.stocks[i] - cum_demand[i]
+        end
         if new_stocks[i] < 0
             println("Error: negative stocks for reference ", i, " !")
         end
@@ -24,11 +30,11 @@ function Compute_Stocks_at_T_plus_1(signal::Signal, production::Production, stoc
 end
 
 
-function Compute_Stocks_for_12_hours(signaux::Vect{Signal}, productions::Vect{Production}, stocks_initiaux::Stocks, nbr_references::Int)
-    Stocks_planning::Vect{Stocks}
-    push!(Stocks_planning, Compute_stocks_at_T_plus_1(signaux[i], productions[i], stocks_initiaux, nbr_references))
+function Compute_Stocks_for_12_hours(signaux::Array{Signal}, productions::Array{Production}, stocks_initiaux::Stocks, nbr_references::Int)
+    Stocks_planning = Array{Stocks}(undef,12)
+    Stocks_planning[1] = Compute_Stocks_at_T_plus_1(signaux[1], productions[1], stocks_initiaux, nbr_references)
     for i in 2:12
-        push!(Stocks_planning, Compute_stocks_at_T_plus_1(signaux[i], productions[i], Stocks_planning[i-1]), nbr_references)
+        Stocks_planning[i] = Compute_Stocks_at_T_plus_1(signaux[i], productions[i], Stocks_planning[i-1], nbr_references)
     end
     return Stocks_planning
 end
